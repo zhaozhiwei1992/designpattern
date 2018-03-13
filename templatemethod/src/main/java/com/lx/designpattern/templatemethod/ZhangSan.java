a9423a64a736071fa612f095bc82f97bc1d1965b
zhao <347061329@qq.com>
2018-03-13 23:39:46 +0800

    update to 手

--- templatemethod/src/main/java/com/lx/designpattern/templatemethod/ZhangSan.java
+++ templatemethod/src/main/java/com/lx/designpattern/templatemethod/ZhangSan.java
@@ -3,14 +3,14 @@ package com.lx.designpattern.templatemethod;
 public class ZhangSan extends Person{
 
     protected void closeTheFridgeDoor() {
-        System.out.println("[张三] 用脚关上冰箱门");
+        System.out.println("[张三] 用手关上冰箱门");
     }
 
     protected void pubElephantIn() {
-        System.out.println("[张三] 用脚把大象踢进冰箱");
+        System.out.println("[张三] 用手把大象踢进冰箱");
     }
 
     protected void openTheFridgeDoor() {
-        System.out.println("[张三] 用脚打开冰箱门");
+        System.out.println("[张三] 用手打开冰箱门");
     }
 }
